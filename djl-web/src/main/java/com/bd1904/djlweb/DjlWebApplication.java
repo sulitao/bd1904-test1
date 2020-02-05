b0832308a6b25ecb5a6ba13a1136658e68d21d5b
sulitao <sltfei@sina.com>
2020-02-05 13:55:46 +0800

    添加点击流的web项目

---
+++ djl-web/src/main/java/com/bd1904/djlweb/DjlWebApplication.java
@@ -0,0 +1,13 @@
+package com.bd1904.djlweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlWebApplication.class, args);
+	}
+
+}
---
+++ djl-web/src/test/java/com/bd1904/djlweb/DjlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd1904.djlweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
